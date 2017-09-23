7b30082fadb960b42a6b23a1801209b8cba307ba
Lau Yan Ting <twilightlau94@outlook.com>
2017-09-23 12:38:55 -0700

    Section 5 - Refactoring Magic String Remarks: found the cause of error is we need to set the MembershipTypeName in the customer model as well. Not implemented yet in this change.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Min18YearsIfAMember]
         public DateTime Birthdate { get; set; }   
     }
 }
